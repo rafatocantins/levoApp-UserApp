250eda809748755c2f52be18d27217f63120fbbb
rafatocantins <rafewebdev@gmail.com>
2019-11-05 12:02:19 +0000

    add changes

--- screens/main/AccountScreen.js
+++ screens/main/AccountScreen.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import firebase from 'firebase';
 
 // get user
 
 const AccountScreen = () => {
+
+    useEffect(() => {
+        console.log(currentUser)
+    }, []);
+    
+    const {currentUser} = firebase.auth()
+    
   return (
-    <View style={styles.container}>
-    <Text>Acount</Text> 
+    <View style={styles.screen}>
+        <View>
+            <Image></Image>
+        </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-  },
   });
 
 export default AccountScreen;
